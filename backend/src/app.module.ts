e716d5d9db4abf2db97202247a840da9e00301fc
MarkOtwane <otwanemark254@gmail.com>
2025-06-18 15:24:41 +0300

    test prisma client and make it to work

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from './admin/admin.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomersModule } from './customers/customers.module';
 import { MailerModule } from './mailer/mailer.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [MailerModule, ConfigModule.forRoot()],
+  imports: [
+    MailerModule,
+    ConfigModule.forRoot(),
+    AdminModule,
+    CustomersModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
