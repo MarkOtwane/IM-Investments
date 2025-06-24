d072672a3b482bc71b9c24535b162053b8a5aa94
MarkOtwane <otwanemark254@gmail.com>
2025-06-24 13:12:55 +0300

    bring the backend and frontend together

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
@@ -8,7 +8,7 @@ import { HeaderComponent } from './shared/components/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent, HttpClientModule, RouterModule],
+  imports: [FooterComponent, HeaderComponent, RouterModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,9 @@
-import {
-  provideHttpClient,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-import { AuthInterceptor } from './app/core/interceptors/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
-  providers: [
-    ...appConfig.providers,
-    provideHttpClient(withInterceptorsFromDi()),
-    AuthInterceptor,
-  ],
-}).catch((err) => console.error(err));
+  providers: [...(appConfig.providers ?? []), provideHttpClient()],
+});
